a99843ce949d55adad2ac15c19ebbe1e0c45606f
Innamute <147324079+Innamute@users.noreply.github.com>
2023-10-10 22:33:04 +0800

    Add files via upload

---
+++ d2.java
@@ -0,0 +1,14 @@
+package mutmainna;
+
+public class d2 {
+
+    public static void main(String[] args) {
+        String nama = "kubus";
+        int angkaPertama = 10;
+        int angkaKedua = 20;
+
+        int hasil = angkaPertama + angkaKedua;
+
+        System.out.println("Hasil penambahan: " + hasil + nama );
+    }
+}
