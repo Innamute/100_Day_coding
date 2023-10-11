966a33b4d66686e3083e58d39b49e4cc966f927b
Innamute <147324079+Innamute@users.noreply.github.com>
2023-10-11 22:38:51 +0800

    Add files via upload

---
+++ d3.java
@@ -0,0 +1,19 @@
+
+package mutmainna;
+
+
+public class d3 {
+     public static void main(String[] args) {
+        int a = 5;
+        int b = 3;
+
+// Operator penjumlahan
+int sum = a + b;
+System.out.println("Hasil penjumlahan: " + sum);
+
+// Operator pengurangan
+int difference = a - b;
+System.out.println("Hasil pengurangan: " + difference);
+    
+    }
+}
