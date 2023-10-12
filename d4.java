e74bc729cf5b6a9d690e3d0c1da3c6fe8322f4b9
Innamute <147324079+Innamute@users.noreply.github.com>
2023-10-12 21:50:00 +0800

    Add files via upload

---
+++ d4.java
@@ -0,0 +1,13 @@
+
+package mutmainna;
+
+public class d4 {
+    
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            System.out.println(i);
+        }
+    }
+}
+    
+
