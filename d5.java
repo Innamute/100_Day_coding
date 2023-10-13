f9dbef8b5918603f611082acb086e0e4b8513cdb
Innamute <147324079+Innamute@users.noreply.github.com>
2023-10-13 22:27:28 +0800

    Add files via upload

---
+++ d5.java
@@ -0,0 +1,14 @@
+
+package mutmainna;
+
+public class d5 {
+    
+    public static void main(String[] args) {
+        String str1 = "Hello";
+        String str2 = "mutmainna";
+        String result = str1 + " " + str2;
+        System.out.println("Hasil penggabungan: " + result);
+    }
+}
+    
+
