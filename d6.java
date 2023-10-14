7a5ede9d673c77c273c6f5f7191b73c8a4ffe023
Innamute <147324079+Innamute@users.noreply.github.com>
2023-10-14 19:17:42 +0800

    Add files via upload

---
+++ d6.java
@@ -0,0 +1,20 @@
+
+package mutmainna;
+
+public class d6 {
+    public static void main(String[] args) {
+        String str1 = "Hello";
+        String str2 = "Hello";
+
+        // Menggunakan operator == untuk memeriksa kesetaraan string
+        boolean isEqual = (str1 == str2);
+
+        // Menampilkan hasil kesetaraan
+        if (isEqual) {
+            System.out.println("str1 sama dengan str2");
+        } else {
+            System.out.println("str1 tidak sama dengan str2");
+        }
+    }
+}
+    
