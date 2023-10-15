ba2458474a1d41bb26b04a39a8664b27f833b7f1
Innamute <147324079+Innamute@users.noreply.github.com>
2023-10-15 19:39:23 +0800

    Add files via upload

---
+++ d7.java
@@ -0,0 +1,16 @@
+
+package mutmainna;
+
+
+public class d7 {
+    public static void main(String[] args) {
+        String str = "Contoh String";
+
+        // Menggunakan operator .length() untuk mendapatkan panjang string
+        int length = str.length();
+
+        // Menampilkan panjang string
+        System.out.println("Panjang string: " + length);
+    }
+}
+    
