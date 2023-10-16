b505ad416ff93c0ab9c5f45acf2e3b2bdc26353b
Innamute <147324079+Innamute@users.noreply.github.com>
2023-10-16 21:41:47 +0800

    Add files via upload

---
+++ d8_operasitipedatafloatingpoint.java
@@ -0,0 +1,14 @@
+
+package mutmainna;
+
+public class d8_operasitipedatafloatingpoint {
+    public static void main(String[] args) {
+        //Tipe data floating-point
+        double x = 10.5;
+        double y = 2.5;
+
+        double product = x * y;
+        System.out.println("Hasil perkalian (double): " + product);
+    }
+    
+}
