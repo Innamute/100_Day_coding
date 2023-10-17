e391499991b6de2356c960e18dbf77b7fe24c9ee
Innamute <147324079+Innamute@users.noreply.github.com>
2023-10-17 23:37:36 +0800

    Add files via upload

---
+++ d9_operatorlogikaAND.java
@@ -0,0 +1,13 @@
+
+package mutmainna;
+
+public class d9_operatorlogikaAND {
+
+    public static void main(String[] args) {
+    boolean a = true;
+    boolean b = false;
+    boolean hasil = a && b; 
+        System.out.println("hasil dari:"+ hasil);
+    }
+}
+
