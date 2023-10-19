a7a616aacf8da6868adadadb2347e9f46bd70dc6
Innamute <147324079+Innamute@users.noreply.github.com>
2023-10-19 21:42:19 +0800

    Add files via upload

---
+++ d11.java
@@ -0,0 +1,14 @@
+
+package mutmainna;
+
+public class d11 {
+    public static void main(String[] args) {
+        // Mendeklarasikan dan menginisialisasi array integer
+        int[] numbers = {1, 2, 3, 4, 5};
+
+        // Mengakses dan mencetak elemen-elemen array
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println("Elemen ke-" + i + ": " + numbers[i]);
+        }
+    }
+}
