28f67280f420f1636c80027b10bf27af8611b94e
Innamute <147324079+Innamute@users.noreply.github.com>
2023-10-22 21:52:08 +0800

    Add files via upload

---
+++ d14.java
@@ -0,0 +1,22 @@
+
+package mutmainna;
+import java.util.ArrayList;
+
+public class d14 {
+    public static void main(String[] args) {
+  
+//membuat ArrayList dengan beberapa elemen
+        ArrayList<Integer> number = new ArrayList<>();
+        number.add(5);
+        number.add(10);
+        number.add(15);
+        number.add(20);
+        number.add(25);
+        number.add(30);
+//        menghitung jumlah elemen dalam ArrayList
+        int count = number.size();
+        System.out.println("jumlah elemen dalam ArrayList: " + count);
+
+    }
+}
+    
