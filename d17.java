3cc0417cf5202d683fef5e87d23e8acee6a7dc19
Innamute <147324079+Innamute@users.noreply.github.com>
2023-10-25 22:28:21 +0800

    Add files via upload

---
+++ d17.java
@@ -0,0 +1,16 @@
+
+package mutmainna;
+
+import java.util.HashSet;
+
+public class d17 {
+    
+    public static void main(String[] args) {
+        HashSet<Double> hashSet = new HashSet<>();
+        hashSet.add(3.14);
+        hashSet.add(1.618);
+        hashSet.add(2.718);
+        System.out.println("Elemen elemn dalam hashSet:" + hashSet);
+    }
+}
+    
