a256eaf9b393191b01ef1228d4ecf87c75fe1abe
Innamute <147324079+Innamute@users.noreply.github.com>
2023-10-29 22:13:18 +0800

    Add files via upload

---
+++ d21.java
@@ -0,0 +1,19 @@
+
+package mutmainna;
+
+import java.util.LinkedList;
+
+public class d21 {
+    public static void main(String[] args) {
+        LinkedList<String> listKata = new LinkedList<String>();
+        listKata.add("kotak");
+        listKata.add("meja");
+        listKata.add("buku");
+        listKata.add("jalan");
+        listKata.add(2, "informatika");
+        listKata.add(2, "Unsulbar");
+        System.out.println(listKata);
+    }
+}
+    
+
