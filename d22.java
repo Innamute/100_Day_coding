91d9e464689793a14c119d8e6041bd56921210f4
Innamute <147324079+Innamute@users.noreply.github.com>
2023-10-30 21:40:06 +0800

    Add files via upload

---
+++ d22.java
@@ -0,0 +1,16 @@
+
+package mutmainna;
+
+import java.util.Stack;
+
+public class d22 {
+    public static void main(String[] args) {
+        Stack<Double> myStack = new Stack<>();
+        myStack.push(23.7);
+        double value = myStack.pop();
+        System.out.println("POP:"+value);
+        System.out.println(myStack);
+    }
+}
+    
+
