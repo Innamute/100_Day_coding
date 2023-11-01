3fbd50d8fbb190c315b87d291f235030bcee2b80
Innamute <147324079+Innamute@users.noreply.github.com>
2023-11-01 20:45:03 +0800

    Add files via upload

---
+++ d24_Queuepoll.java
@@ -0,0 +1,17 @@
+
+package mutmainna;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class d24_Queuepoll {
+    public static void main(String[] args) {
+        Queue<Double> myQueue = new LinkedList<>();
+        myQueue.offer(14.32);
+        myQueue.offer(22.30);
+        double value = myQueue.poll();
+        System.out.println("POLL" + value);
+        System.out.println(myQueue);
+
+    }
+}
