81afecc145de2fd27173b7771cd9ea856ac6516a
Innamute <147324079+Innamute@users.noreply.github.com>
2023-11-04 22:14:02 +0800

    Add files via upload

---
+++ d27_TipeDataPrimitif.java
@@ -0,0 +1,25 @@
+
+public class mute {
+
+    public static void main(String[] args) {
+//        tipe data primitif integer
+        int angkaBulat = 10;
+        long angkaPanjang = 1000000000L;
+//tipe data primitif floating-point
+        float angkahPecahan = 3.14f;
+        double angkaPecahanLebihTepat = 3.146666667;
+//tipe data primitif karakter
+        char karakter = 'A';
+//tipe data primitif boolean
+        boolean benar = true;
+        boolean salah = false;
+
+        System.out.println("angka bulat:" + angkaBulat);
+        System.out.println("angka panjang:" + angkaPanjang);
+        System.out.println("angka pecahan lebih tepat:" + angkaPecahanLebihTepat);
+        System.out.println("karakter:" + karakter);
+        System.out.println("benar:" + benar);
+        System.out.println("salah:" + salah);
+
+    }
+}
