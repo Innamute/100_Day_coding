86a7ba2507f17f68c2b698a90757b7f85ab345ed
Innamute <147324079+Innamute@users.noreply.github.com>
2023-11-07 23:12:09 +0800

    Add files via upload

---
+++ d30.java
@@ -0,0 +1,17 @@
+
+import java.util.Scanner;
+
+
+public class d30 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("masukkan nilai  : ");
+        int nilai = sc.nextInt();
+        for (int i = nilai ; i >= 0; i--) {
+            System.out.println(i);
+            
+        }
+    }
+}
+    
+
