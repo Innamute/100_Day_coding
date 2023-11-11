81b086008c1aa768ad2493ddfa64fdda208ec587
Innamute <147324079+Innamute@users.noreply.github.com>
2023-11-11 22:30:43 +0800

    Add files via upload

---
+++ d34_MenghitungVolumeBalok.java
@@ -0,0 +1,14 @@
+
+public class d34_MenghitungVolumeBalok {
+
+    public static void main(String[] args) {
+        // Menentukan panjang sisi kubus
+        int sisi = 30; // Ganti dengan panjang sisi yang Anda inginkan
+
+        // Menghitung volume kubus
+        int volume = sisi * sisi * sisi;
+
+        // Menampilkan hasil
+        System.out.println("Volume kubus adalah: " + volume);
+    }
+}
