c972cf5c2261af6ed4518f729effbb5cfb348f35
Innamute <147324079+Innamute@users.noreply.github.com>
2023-11-12 22:41:47 +0800

    Add files via upload

---
+++ d35_LuasSegitiga.java
@@ -0,0 +1,20 @@
+
+import java.util.Scanner;
+
+public class d35_LuasSegitiga {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("masukkan alas : ");
+        int alas = sc.nextInt();
+
+        System.out.print("masukkan tinggi ");
+        int tinggi = sc.nextInt();
+
+        int hasil = alas * tinggi;
+        int hasil2 = hasil * 1 / 2;
+
+        System.out.println("maka hasilnya " + hasil2);
+    }
+}
