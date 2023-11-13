84a7414d23444d7bdfbaeda1380e03e5e81f8a9b
Innamute <147324079+Innamute@users.noreply.github.com>
2023-11-13 23:44:57 +0800

    Add files via upload

---
+++ d36_boolean.java
@@ -0,0 +1,12 @@
+
+public class d36_boolean {
+
+    public static void main(String[] args) {
+        int angka1 = 12;
+        int angka2 = 12;
+
+        boolean hasil = angka1 != angka2;
+        System.out.println("apakah angka 1 tidak sama dengan angka 2" + hasil);
+    }
+
+}
