f767f99a4cb4094c97d4899902a237d9effd1d5a
Innamute <147324079+Innamute@users.noreply.github.com>
2023-11-14 23:37:22 +0800

    Add files via upload

---
+++ d37_SisaBagi.java
@@ -0,0 +1,11 @@
+
+public class d37_SisaBagi {
+
+    public static void main(String[] args) {
+        int buku = 20;
+        int pulpen = 7;
+        int hasil = buku % pulpen;
+        System.out.println("Hasil Bagi dari buku dan pulpen : " + hasil);
+    }
+
+}
