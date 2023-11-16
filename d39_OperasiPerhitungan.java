65c2e23fc1c5600377fb9a101858eb353c19fd0e
Innamute <147324079+Innamute@users.noreply.github.com>
2023-11-16 22:22:59 +0800

    Add files via upload

---
+++ d39_OperasiPerhitungan.java
@@ -0,0 +1,18 @@
+
+public class d39_OperasiPerhitungan {
+
+    public static void main(String[] args) {
+        int A = 200;
+        int B = 68;
+
+        int hasil = A * B;
+        int hasil1 = A + B;
+        int hasil2 = A / B;
+        int hasil3 = A - B;
+
+        System.out.println("Hasil Perkalian : " + hasil);
+        System.out.println("Hasil Penjumlahan : " + hasil1);
+        System.out.println("Hasil Pembagian : " + hasil2);
+        System.out.println("Hasil Pengurangan : " + hasil3);
+    }
+}
