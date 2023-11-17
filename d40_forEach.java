4b75110321465b188fb08621d7881b1a387cc1c9
Innamute <147324079+Innamute@users.noreply.github.com>
2023-11-17 22:55:02 +0800

    Add files via upload

---
+++ d40_forEach.java
@@ -0,0 +1,12 @@
+
+public class d40_forEach {
+    public static void main(String[] args) {
+        int[] numbers = {11, 12, 13, 14, 15};
+
+        for (int number : numbers) {
+            System.out.println(number);
+        }
+    }
+    }
+    
+
