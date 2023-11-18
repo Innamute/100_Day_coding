5743a61b4d1de660ccbeea704151fa48587088a1
Innamute <147324079+Innamute@users.noreply.github.com>
2023-11-18 23:08:44 +0800

    Add files via upload

---
+++ d41_MenghitungPangkatMenggunakanForEach.java
@@ -0,0 +1,17 @@
+
+public class d41_MenghitungPangkatMenggunakanForEach {
+    public static void main(String[] args) {
+        /*Tulislah program dengan bahasa java untuk menghitung hasil dari  3^4 !
+        *Gunakan konsep perulangan untuk menyelesaikannya
+    */    
+    
+        int agk = 3;
+        for (int i = 0; i <= 3; i++) {
+            agk*= 3;
+        }
+         System.out.println(agk);
+        }   
+    
+}
+    
+
