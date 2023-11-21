6c0802dd9e19c367acaeb7752d6f1db182414d79
Innamute <147324079+Innamute@users.noreply.github.com>
2023-11-21 23:44:37 +0800

    Add files via upload

---
+++ d44_PolaSegiEmpat.java
@@ -0,0 +1,16 @@
+
+public class d44_PolaSegiEmpat {
+    public static void main(String[] args) {
+           int angka = 3 ;
+        for (int i = 1; i <= angka ; i++) {
+            for (int j = 1; j < angka; j++) {
+                System.out.print("* ");
+            }
+            System.out.println("* ");
+            
+        }
+        System.out.println("");
+    }
+    
+}
+  
